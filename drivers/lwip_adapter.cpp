a22a50719e0fcaca77e53a09759e52306ea38481
gurijb-ftk <gurijb2016@gmail.com>
2026-08-01 12:20:37 +0200

    Fix: cast L"..." to (CHAR16*) when calling Print to match EFI CHAR16 type

--- drivers/lwip_adapter.cpp
+++ drivers/lwip_adapter.cpp
@@ -8,10 +8,10 @@ void lwip_adapter::init() {
     // lwip_adapter::transmit() for sending frames and call netif->input() when data arrives.
     // This scaffold leaves lwIP out of tree; the adapter provides the integration points.
     if (!virtio_net::init()) {
-        Print(L"lwip_adapter: virtio-net not available, lwIP cannot be attached\n");
+        Print((CHAR16*)L"lwip_adapter: virtio-net not available, lwIP cannot be attached\n");
         return;
     }
-    Print(L"lwip_adapter: initialized (adapter only)\n");
+    Print((CHAR16*)L"lwip_adapter: initialized (adapter only)\n");
 }
 
 void lwip_adapter::poll_receive() {
@@ -19,7 +19,7 @@ void lwip_adapter::poll_receive() {
     int r = virtio_net::receive_packet(tmp, sizeof(tmp));
     if (r > 0) {
         // in a real integration we'd pass this packet to lwIP (e.g., netif->input)
-        Print(L"lwip_adapter: received packet (len)\n");
+        Print((CHAR16*)L"lwip_adapter: received packet (len)\n");
     }
 }
 
