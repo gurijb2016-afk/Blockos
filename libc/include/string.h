110b8e3c9ed026936fbbd0a95c63e0e49064df87
gurijb-ftk <gurijb2016@gmail.com>
2026-08-21 16:39:39 +0200

    Refactor string.h to simplify function declarations

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,81 +6,27 @@
 extern "C" {
 #endif
 
-void* memcpy(
-    void* destination,
-    const void* source,
-    size_t size
-);
+void* memcpy(void* dst, const void* src, size_t n);
+void* memmove(void* dst, const void* src, size_t n);
+void* memset(void* dst, int c, size_t n);
+int memcmp(const void* a, const void* b, size_t n);
 
-void* memmove(
-    void* destination,
-    const void* source,
-    size_t size
-);
+size_t strlen(const char* s);
 
-void* memset(
-    void* destination,
-    int value,
-    size_t size
-);
+int strcmp(const char* a, const char* b);
+int strncmp(const char* a, const char* b, size_t n);
 
-int memcmp(
-    const void* a,
-    const void* b,
-    size_t size
-);
+char* strcpy(char* dst, const char* src);
+char* strncpy(char* dst, const char* src, size_t n);
 
-size_t strlen(const char* str);
+char* strcat(char* dst, const char* src);
+char* strncat(char* dst, const char* src, size_t n);
 
-int strcmp(
-    const char* a,
-    const char* b
-);
+char* strchr(const char* s, int c);
+char* strrchr(const char* s, int c);
+char* strstr(const char* haystack, const char* needle);
 
-int strncmp(
-    const char* a,
-    const char* b,
-    size_t n
-);
-
-char* strcpy(
-    char* destination,
-    const char* source
-);
-
-char* strncpy(
-    char* destination,
-    const char* source,
-    size_t n
-);
-
-char* strcat(
-    char* destination,
-    const char* source
-);
-
-char* strncat(
-    char* destination,
-    const char* source,
-    size_t n
-);
-
-char* strchr(
-    const char* str,
-    int character
-);
-
-char* strrchr(
-    const char* str,
-    int character
-);
-
-char* strstr(
-    const char* haystack,
-    const char* needle
-);
-
-char* strdup(const char* str);
+char* strdup(const char* s);
 
 #ifdef __cplusplus
 }
