33adc4a873388d0691ef54a08f1f550bc679e4f6
gurijb-ftk <gurijb2016@gmail.com>
2026-08-21 16:41:41 +0200

    Create math.h with function prototypes and M_PI
    Added mathematical function declarations and constant.

---
+++ libc/include/math.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double sqrt(double x);
+double fabs(double x);
+double sin(double x);
+double cos(double x);
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#ifdef __cplusplus
+}
+#endif
