101082e9aa8e23d8e465e99c270034fd9e3997ae
gurijb-ftk <gurijb2016@gmail.com>
2026-09-04 14:36:42 +0200

    Refactor PciDevice and PciSubsystem structures

--- drivers/pci_subsystem.hpp
+++ drivers/pci_subsystem.hpp
@@ -1,33 +1,88 @@
 #pragma once
+
 #include <stdint.h>
 
-struct PciDevice {
+static constexpr uint32_t PCI_MAX_DEVICES = 64;
+
+struct PciDevice
+{
     uint8_t  bus;
     uint8_t  slot;
     uint8_t  func;
+
     uint16_t vendor_id;
     uint16_t device_id;
+
     uint8_t  class_id;
     uint8_t  subclass;
-    uint32_t bar0;
-    uint32_t bar1;
+    uint8_t  prog_if;
+    uint8_t  revision;
+
+    uint8_t  header_type;
+
+    uint64_t bar[6];
+
     bool     is_valid;
 };
 
-class PciSubsystem {
+class PciSubsystem
+{
 private:
-    PciDevice device_registry[64];
-    uint32_t  registered_count;
+    PciDevice device_registry[PCI_MAX_DEVICES];
+    uint32_t registered_count;
 
-    uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
-    uint32_t pci_config_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+    uint32_t pci_config_read_dword(
+        uint8_t bus,
+        uint8_t slot,
+        uint8_t func,
+        uint8_t offset
+    );
+
+    uint16_t pci_config_read_word(
+        uint8_t bus,
+        uint8_t slot,
+        uint8_t func,
+        uint8_t offset
+    );
+
+    void clear_registry();
+
+    void scan_function(
+        uint8_t bus,
+        uint8_t slot,
+        uint8_t func
+    );
 
 public:
     PciSubsystem();
+
     void scan_all_pci_buses();
+
+    const PciDevice* get_devices(
+        uint32_t* count
+    ) const;
+
+    PciDevice* get_device_by_id(
+        uint16_t vendor,
+        uint16_t device
+    );
+
+    PciDevice* get_device(
+        uint8_t bus,
+        uint8_t slot,
+        uint8_t func
+    );
+
+    uint32_t device_count() const;
+
     void configure_mmio_bars();
-    void print_hardware_tree_to_gui(uint8_t* bb, uint32_t fb_w, int win_x, int win_y);
-    PciDevice* get_device_by_id(uint16_t vendor, uint16_t device);
+
+    void print_hardware_tree_to_gui(
+        uint8_t* bb,
+        uint32_t fb_w,
+        int win_x,
+        int win_y
+    );
 };
 
 extern PciSubsystem pci_bus_manager;
