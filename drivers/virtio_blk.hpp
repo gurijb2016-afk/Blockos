e00db7361ec23d499d4b85a96a44702ec61a8620
gurijb-ftk <gurijb2016@gmail.com>
2026-09-04 16:28:37 +0200

    Refactor virtio_blk to virtio_blk_full class
    Refactored virtio_blk functions into virtio_blk_full class with read and write sector methods.

--- drivers/virtio_blk.hpp
+++ drivers/virtio_blk.hpp
@@ -1,8 +1,42 @@
 #pragma once
-#include <stdint.h>
 
-namespace virtio_blk {
-    bool init();
-    bool read_sector(uint64_t sector, uint8_t* out_buf);
-    bool write_sector(uint64_t sector, const uint8_t* in_buf);
-}
+#include <cstdint>
+
+class virtio_blk_full {
+public:
+    bool read_sector(
+        uint64_t sector,
+        uint8_t* out_buf
+    )
+    {
+        return read_sectors(
+            sector,
+            1,
+            out_buf
+        );
+    }
+
+    bool write_sector(
+        uint64_t sector,
+        const uint8_t* in_buf
+    )
+    {
+        return write_sectors(
+            sector,
+            1,
+            in_buf
+        );
+    }
+
+    bool read_sectors(
+        uint64_t sector,
+        uint32_t count,
+        uint8_t* out_buf
+    );
+
+    bool write_sectors(
+        uint64_t sector,
+        uint32_t count,
+        const uint8_t* in_buf
+    );
+};
