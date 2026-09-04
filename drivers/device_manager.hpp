53ce1f8909473c4ad76f8b5185b5c1ed566d94e4
gurijb-ftk <gurijb2016@gmail.com>
2026-09-04 14:38:02 +0200

    Increase MAX_DEVICES and expand DeviceDescriptor

--- drivers/device_manager.hpp
+++ drivers/device_manager.hpp
@@ -1,47 +1,104 @@
 #pragma once
+
 #include <stdint.h>
 #include <stddef.h>
 
-// Maximálisan egyidejűleg kezelhető hardvereszközök száma
-#define MAX_DEVICES 16
+#define MAX_DEVICES 64
 
-// Eszközkategóriák definíciói
-enum DeviceType {
-    DEV_TYPE_UNKNOWN,
-    DEV_TYPE_STORAGE,   // VirtIO-Blk, Merevlemezek
-    DEV_TYPE_NETWORK,   // VirtIO-Net, Hálózati kártyák
-    DEV_TYPE_GRAPHICS,  // UEFI Framebuffer, Videókártyák
-    DEV_TYPE_INPUT      // PS/2 Egér, Billentyűzet
+enum DeviceType
+{
+    DEV_TYPE_UNKNOWN = 0,
+    DEV_TYPE_STORAGE,
+    DEV_TYPE_NETWORK,
+    DEV_TYPE_GRAPHICS,
+    DEV_TYPE_INPUT,
+    DEV_TYPE_USB,
+    DEV_TYPE_AUDIO,
+    DEV_TYPE_PCI
 };
 
-// Egy konkrét eszköz hardveres leíró struktúrája
-struct DeviceDescriptor {
-    uint32_t   id;
-    char       name[32];
+struct DeviceDescriptor
+{
+    uint32_t id;
+
+    char name[32];
+
     DeviceType type;
-    uint64_t   io_base_addr; // MMIO fizikai báziscím a memóriában
-    uint8_t    irq_vector;   // CPU megszakítási vektor száma
-    bool       is_ready;
+
+    uint64_t io_base_addr;
+
+    uint8_t irq_vector;
+
+    uint16_t pci_vendor_id;
+    uint16_t pci_device_id;
+
+    uint8_t pci_bus;
+    uint8_t pci_slot;
+    uint8_t pci_func;
+
+    bool is_present;
+    bool is_ready;
 };
 
-class DeviceManager {
+class DeviceManager
+{
 private:
     DeviceDescriptor device_list[MAX_DEVICES];
-    uint32_t         total_devices;
+    uint32_t total_devices;
 
-    void local_strcpy(char* dest, const char* src, size_t max_len);
+    void clear_device(
+        DeviceDescriptor& device
+    );
+
+    void local_strcpy(
+        char* dest,
+        const char* src,
+        size_t max_len
+    );
 
 public:
     DeviceManager();
 
-    // Új hardver eszköz regisztrálása a rendszermagba
-    bool register_device(const char* name, DeviceType type, uint64_t io_base, uint8_t irq);
-    
-    // Eszköz keresése kategória alapján
-    DeviceDescriptor* find_device_by_type(DeviceType type);
-    
-    // Hardverek inicializálása és diagnosztikai jelentés kiírása a GUI-ra
+    bool register_device(
+        const char* name,
+        DeviceType type,
+        uint64_t io_base,
+        uint8_t irq
+    );
+
+    bool register_pci_device(
+        const char* name,
+        DeviceType type,
+        uint8_t bus,
+        uint8_t slot,
+        uint8_t func,
+        uint16_t vendor,
+        uint16_t device,
+        uint64_t io_base,
+        uint8_t irq
+    );
+
+    DeviceDescriptor* find_device_by_type(
+        DeviceType type
+    );
+
+    DeviceDescriptor* find_device(
+        const char* name
+    );
+
+    const DeviceDescriptor* devices(
+        uint32_t* count
+    ) const;
+
+    uint32_t count() const;
+
+    bool set_ready(
+        uint32_t id,
+        bool ready
+    );
+
     void initialize_all_hardware();
+
     void show_device_status_report();
 };
 
