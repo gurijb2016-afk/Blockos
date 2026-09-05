3d7b1150fed67c52cbb94e7542bfc1f6875f2ab8
gurijb-ftk <gurijb2016@gmail.com>
2026-09-05 07:54:52 +0200

    Refactor lua_elf.cpp to use ramfs.h

--- fs/lua_elf.cpp
+++ fs/lua_elf.cpp
@@ -1,14 +1,10 @@
 
-#include "vfs.hpp"
+#include "ramfs.h"
 
 #include <stdint.h>
-#include <stddef.h>
 
-extern "C"
-{
-    extern const unsigned char __lua_elf_start[];
-    extern const unsigned char __lua_elf_end[];
-}
+extern "C" const uint8_t __lua_elf_start[];
+extern "C" const uint8_t __lua_elf_end[];
 
 asm(
     ".section .rodata\n"
@@ -22,25 +18,12 @@ asm(
 );
 
 extern "C"
+const struct ramfile __ramfs_lua[]
+    __attribute__((section(".ramfs"), used, aligned(8))) =
 {
-    const ramfile __ramfs_lua
-        __attribute__((section(".ramfs"), used, aligned(8))) =
     {
         "/bin/lua",
         __lua_elf_start,
-        static_cast<uint32_t>(
-            __lua_elf_end - __lua_elf_start
-        )
-    };
-}
-
-extern "C" const ramfile __ramfs_end[];
-
-asm(
-    ".section .ramfs\n"
-    ".balign 8\n"
-    ".global __ramfs_end\n"
-    "__ramfs_end:\n"
-    ".previous\n"
-);
-
+        (uint32_t)(__lua_elf_end - __lua_elf_start)
+    }
+};
