35866cb6b36a36e93d041cce0d917da1cf80c700
gurijb-ftk <gurijb2016@gmail.com>
2026-09-25 19:28:08 +0200

    Delete ports/gnome42 directory

--- ports/gnome42/build-1-2-3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-BASE=$(CDPATH= cd -- "$(dirname "$0")/../.." && pwd)
-"$BASE/ports/gnome42/fetch-sources.sh"
-"$BASE/ports/gnome42/build-libffi.sh"
-"$BASE/ports/gnome42/build-glib.sh"
-"$BASE/ports/gnome42/build-cairo.sh"
-printf '%s\n' 'GLib/GObject/GIO + libffi + Cairo build sequence completed.'
--- ports/gnome42/compile-schemas.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-SCHEMAS=${1:-/System/share/glib-2.0/schemas}
-exec /System/bin/glib-compile-schemas "$SCHEMAS"
